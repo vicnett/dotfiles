46ce685bdca3e13ab5e213799123d6ec882df80a
Vincent Capelle <vicnett@gmail.com>
2024-10-27 02:51:22 -0400

    Tweak setup.sh not to require being in any specific directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
+
+DOTFILES_DIR="$HOME/.dotfiles"
+PLAYBOOK_PATH="$DOTFILES_DIR/ansible/playbook.yml"
 
 sudo pacman -S --needed --noconfirm git python-pipx
 export PATH="$PATH:~/.local/bin"
 pipx install --include-deps ansible
 
-ansible-playbook ansible/playbook.yml
+if [ ! -d $DOTFILES_DIR ]
+then
+  git clone https://github.com/vicnett/dotfiles.git $DOTFILES_DIR
+fi
+
+if [ -f $PLAYBOOK_PATH ]
+then
+  ansible-playbook $PLAYBOOK_PATH
+else
+  echo Ansible playbook not found at $PLAYBOOK_PATH
+  exit 1
+fi
