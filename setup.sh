78facac326ce988b185673cdee90dd3d5730cc84
Vincent Capelle <vicnett@gmail.com>
2024-10-27 21:15:58 -0400

    Add simple docker build workflow

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 set -e
 
 DOTFILES_DIR="$HOME/.dotfiles"
+ANSIBLE_PATH="$HOME/.local/bin"
 PLAYBOOK_PATH="$DOTFILES_DIR/ansible/playbook.yml"
 
 sudo pacman -S --needed --noconfirm git python-pipx
-export PATH="$PATH:~/.local/bin"
 pipx install --include-deps ansible
 
 if [ ! -d $DOTFILES_DIR ]
@@ -16,7 +16,7 @@ fi
 
 if [ -f $PLAYBOOK_PATH ]
 then
-  ansible-playbook $PLAYBOOK_PATH
+  $ANSIBLE_PATH/ansible-playbook $PLAYBOOK_PATH
 else
   echo Ansible playbook not found at $PLAYBOOK_PATH
   exit 1
