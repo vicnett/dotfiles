a57ebec9574f49730abe96047b92a63f99db262e
Vincent Capelle <vicnett@gmail.com>
2024-11-10 14:38:47 -0500

    fix: re-indent existing Lua code to use spaces

--- vim/lua/plugins/text-objects.lua
+++ vim/lua/plugins/text-objects.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"kana/vim-textobj-user",
-		priority = 100,
-	},
-	{ "kana/vim-textobj-line" },
-	{ "kana/vim-textobj-indent" },
-	{ "kana/vim-textobj-entire" },
-	{ "beloglazov/vim-textobj-quotes" },
-	{ "christoomey/vim-textobj-codeblock" },
+  {
+    "kana/vim-textobj-user",
+    priority = 100,
+  },
+  { "kana/vim-textobj-line" },
+  { "kana/vim-textobj-indent" },
+  { "kana/vim-textobj-entire" },
+  { "beloglazov/vim-textobj-quotes" },
+  { "christoomey/vim-textobj-codeblock" },
 }
