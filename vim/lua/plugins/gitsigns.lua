66d20be588a7f62a9b0c14712d8331908721b8e1
Vincent Capelle <vicnett@gmail.com>
2024-11-15 17:40:35 -0500

    chore: Add missing keymap descriptions

--- vim/lua/plugins/gitsigns.lua
+++ vim/lua/plugins/gitsigns.lua
@@ -20,7 +20,7 @@ return {
             else
               gitsigns.nav_hunk("next")
             end
-          end)
+          end, { desc = "Next change" } )
 
           map("n", "[c", function()
             if vim.wo.diff then
@@ -28,30 +28,70 @@ return {
             else
               gitsigns.nav_hunk("prev")
             end
-          end)
+          end, { desc = "Previous change" } )
 
           -- Actions
-          map("n", "<leader>hs", gitsigns.stage_hunk)
-          map("n", "<leader>hr", gitsigns.reset_hunk)
+          map(
+            "n",
+            "<leader>hs",
+            gitsigns.stage_hunk,
+            { desc = "[h]unk [s]tage" }
+          )
+          map(
+            "n",
+            "<leader>hr",
+            gitsigns.reset_hunk,
+            { desc = "[h]unk [r]eset" }
+          )
           map("v", "<leader>hs", function()
             gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
+          end, { desc = "[h]unk [s]tage" })
           map("v", "<leader>hr", function()
             gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
-          map("n", "<leader>hS", gitsigns.stage_buffer)
-          map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-          map("n", "<leader>hR", gitsigns.reset_buffer)
-          map("n", "<leader>hp", gitsigns.preview_hunk)
+          end, { desc = "[h]unk [r]eset" })
+          map(
+            "n",
+            "<leader>hS",
+            gitsigns.stage_buffer,
+            { desc = "[h]unk [S]tage (entire buffer)" }
+          )
+          map(
+            "n",
+            "<leader>hu",
+            gitsigns.undo_stage_hunk,
+            { desc = "[h]unk [u]ndo stage" }
+          )
+          map(
+            "n",
+            "<leader>hR",
+            gitsigns.reset_buffer,
+            { desc = "[h]unk [R]eset (entire buffer)" }
+          )
+          map(
+            "n",
+            "<leader>hp",
+            gitsigns.preview_hunk,
+            { desc = "[h]unk [p]review" }
+          )
           map("n", "<leader>hb", function()
             gitsigns.blame_line({ full = true })
-          end)
-          map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-          map("n", "<leader>hd", gitsigns.diffthis)
+          end, { desc = "[h]unk [b]lame" })
+          map(
+            "n",
+            "<leader>tb",
+            gitsigns.toggle_current_line_blame,
+            { desc = "[t]oggle [b]lame (virtual text)" }
+          )
+          map("n", "<leader>hd", gitsigns.diffthis, { desc = "[h]unk [d]iff" })
           map("n", "<leader>hD", function()
             gitsigns.diffthis("~")
-          end)
-          map("n", "<leader>td", gitsigns.toggle_deleted)
+          end, { desc = "[h]unk [D]iff (entire buffer)" })
+          map(
+            "n",
+            "<leader>td",
+            gitsigns.toggle_deleted,
+            { desc = "[t]oggle show [d]iff (virtual text)" }
+          )
 
           -- Text object
           map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
