fd680f0db0d8c372c5291407e796b6b96f901b0d
Vincent Capelle <vicnett@gmail.com>
2025-03-01 18:38:58 -0500

    feat: add keymaps for opening quickfix list and floating window

--- vim/lua/config/remaps.lua
+++ vim/lua/config/remaps.lua
@@ -7,3 +7,17 @@ vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 -- and in visual mode
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+-- Diagnostic keymaps
+vim.keymap.set(
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { desc = "Open diagnostic [q]uickfix list" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>Q",
+  vim.diagnostic.open_float,
+  { desc = "Open diagnostic [Q]uickfix floating window" }
+)
